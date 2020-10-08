7d68f440cd50d018f20a583d6afc48f96fa3295b
Elena Kharitonova <ekhar@ad.unc.edu>
2020-10-08 00:57:10 +0000

    Added figure 1

--- figuresreport1.R
+++ figuresreport1.R
@@ -27,4 +27,57 @@ twv$wine_type<- factor(twv$wine_type,levels = c("red", "white", "rose", "other w
 ## Filter out other non-wines
 onlywine = twv[twv$wine_type != "other alcohol", ]
 
+## Make text lower case
+onlywine$reviews.text = tolower(onlywine$reviews.text)
+
+## Find most common words used
+writing = onlywine$reviews.text
+writing = onlywine$reviews.text
+onlywine$reviews.text = tolower(onlywine$reviews.text)
+data_frame(writing = writing) %>% 
+  unnest_tokens(word, writing) %>%    # split words
+  anti_join(stop_words) %>%    # take out "a", "an", "the", etc.
+  count(word, sort = TRUE)    # count occurrences
+## words common are sweet, price, smooth, dry, light, flavorful, fruit, 
+## and affordable
+
+## Make vector with all the common words
+word = c("sweet", "price", "smooth", "dry", "light", "fruit", "cheap", "flavorful", "affordable")
+
+## Add columns to test which reviews have the most common word
+wineswtraits = onlywine %>% mutate(sweet = str_detect(reviews.text, "sweet"),
+                                   price = str_detect(reviews.text, "price"),
+                                   smooth = str_detect(reviews.text, "smooth"),
+                                   dry = str_detect(reviews.text, "dry"),
+                                   light = str_detect(reviews.text, "light"),
+                                   fruit = str_detect(reviews.text, "fruit"),
+                                   cheap = str_detect(reviews.text, "cheap"),
+                                   flavorful = str_detect(reviews.text, "flavorful"),
+                                   affordable = str_detect(reviews.text, "affordable"))
+
+## Calculate average and standard deviation of rating by word
+averageratingbyword = c()
+for (i in 34:42){
+  averageratingbyword[i-33] = mean(wineswtraits$reviews.rating[unlist(wineswtraits[,i])], na.rm = TRUE)
+}
+sdratingbyword = c()
+for (i in 34:42){
+  sdratingbyword[i-33] = sd(wineswtraits$reviews.rating[unlist(wineswtraits[,i])], na.rm = TRUE)
+}
+
+## Make dataframe that include average rating and sd by word
+wordsandrating = data.frame(word=word, mean = averageratingbyword, sd = sdratingbyword)
+wordsandrating = wordsandrating %>% arrange(desc(mean))
+
+## Make graph
+figure1 = ggplot(wordsandrating, aes(x=word, y = mean)) + geom_bar(stat = "identity") +
+  geom_errorbar(aes(x=word, ymin=mean- sd, ymax=mean + sd), width=0.3, alpha=0.9, size=.8) + 
+  theme_classic()
+
+png("report1figures/figure1.png", width = 800, height = 600)
+print(figure1)
+dev.off()
+
+
+
 
