992b41aeb9b3f1f917a94f71972c4dfc135d27b1
Elena Kharitonova <ekhar@ad.unc.edu>
2020-11-10 04:00:19 +0000

    Added comments to make code clearer

--- integration_fig_gen.py
+++ integration_fig_gen.py
@@ -1,3 +1,5 @@
+## Load libraries
+
 import numpy as np
 import pandas as pd
 from sklearn.manifold import TSNE
@@ -10,33 +12,51 @@ from sklearn.cluster import KMeans
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.inspection import partial_dependence
 from sklearn.inspection import plot_partial_dependence
+
+
+
+## Load files
+
 winequality_red = pd.read_csv("source_data/winequality-red.csv")
 winequality_white = pd.read_csv("source_data/winequality-white.csv")
-y = winequality_red["quality"]
+
+
+
+## Create data fram with only those columns used for gbm model 
+
 winequality_red1 = winequality_red[['alcohol', 'residual sugar', 'pH', 'fixed acidity', 'free sulfur dioxide', 'volatile acidity']]
 winequality_white1 = winequality_white[['alcohol', 'residual sugar', 'pH', 'fixed acidity', 'free sulfur dioxide', 'volatile acidity']]
+
+
+
+## Create gbm for red wine
+
+y = winequality_red["quality"]
 my_model = GradientBoostingRegressor()
 my_model.fit(winequality_red1, y)
-my_plots1 = plot_partial_dependence(my_model,       
-                                   features=[0, 5], # column numbers of plots we want to show
-                                   X=winequality_red1,            # raw predictors data.
-                                   feature_names=['Red Wine Alcohol Content', 'residual sugar', 'pH', 'fixed acidity', 'free sulfur dioxide', 'Red Wine Volatile Acidity Content'], # labels on graphs
-                                   grid_resolution=50)
+
+
+
+## Create gbm for white wine
+
 my_model1 = GradientBoostingRegressor()
 y1 = winequality_white["quality"]
 my_model1.fit(winequality_white1, y1)
-my_plots2 = (plot_partial_dependence(my_model1,       
-                                   features=[0, 5], # column numbers of plots we want to show
-                                   X=winequality_white1,            # raw predictors data.
-                                   feature_names=['White Wine Alcohol Content', 'residual sugar', 'pH', 'fixed acidity', 'free sulfur dioxide', 'White Wine Volatile Acidity Content'], # labels on graphs
-                                   grid_resolution=50))
+
+
+## Save partial dependency plot for red wine
+
 plt.figure()
 plot_partial_dependence(my_model,       
                                    features=[0, 5], # column numbers of plots we want to show
                                    X=winequality_red1,            # raw predictors data.
                                    feature_names=['Red Wine Alcohol Content', 'residual sugar', 'pH', 'fixed acidity', 'free sulfur dioxide', 'Red Wine Volatile Acidity Content'], # labels on graphs
                                    grid_resolution=50)
-plt.savefig('pythonfigure/red_wine_partial_dependency.png', format = 'png')
+plt.savefig('pythonfigure/red_wine_partial_dependency.png', format = 'png')                                   
+                                   
+
+## Save partial dependency plot for white wine
+
 plt.figure()
 plot_partial_dependence(my_model1,       
                                    features=[0, 5], # column numbers of plots we want to show
